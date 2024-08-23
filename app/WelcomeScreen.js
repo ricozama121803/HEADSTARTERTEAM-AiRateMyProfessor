e20c926b5a5eada0944236834ad6c23f6ad17683
Maryam Sikander <119351721+Maryam-Sikander@users.noreply.github.com>
2024-08-23 05:47:30 -0700

    UI changes

--- app/WelcomeScreen.js
+++ app/WelcomeScreen.js
@@ -58,7 +58,7 @@ export default function WelcomeScreen({ onFinish }) {
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1.5, ease: 'easeOut' }}
-       >FindMy Professor</Typography>
+       >Find My Professor</Typography>
       </Box>
     </motion.div>
   );
