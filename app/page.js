be5c399919a58336f3e73a793693552498796140
Enrico Zamarripa <ricozama121803@gmail.com>
2024-08-27 16:37:16 -0700

    favicon + UI edit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,8 @@ import { ArrowUpward } from '@mui/icons-material';
 import ReactMarkdown from 'react-markdown';
 import { motion } from 'framer-motion';
 import WelcomeScreen from './WelcomeScreen';
+import Head from 'next/head'; // Import this if using Next.js
+
 
 export default function Home() {
   const [showWelcome, setShowWelcome] = useState(true);
@@ -73,6 +75,9 @@ export default function Home() {
 
   return (
     <Box>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       {showWelcome && <WelcomeScreen onFinish={() => setShowWelcome(false)} />}
       <Box
         sx={{
@@ -237,33 +242,35 @@ export default function Home() {
             </Stack>
           </motion.div>
         </Box>
-        <Box
-          component="a"
-          href="https://github.com/ricozama121803/HEADSTARTERTEAM-AiRateMyProfessor"
-          target="_blank"
-          sx={{
-            position: 'fixed',
-            bottom: 20,
-            right: 20,
-            zIndex: 1000,
-            cursor: 'pointer',
-            color: '#a3aab5',
-            '&:hover': {
-              color: '#555',
-            },
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            width="50"
-            height="50"
-            fill="currentColor"
-          >
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.82a7.68 7.68 0 0 1 2-.27c.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-          </svg>
-        </Box>
-      </Box>
+        {contentVisible && (
+    <Box
+      component="a"
+      href="https://github.com/ricozama121803/HEADSTARTERTEAM-AiRateMyProfessor"
+      target="_blank"
+      sx={{
+        position: 'fixed',
+        bottom: 20,
+        right: 20,
+        zIndex: 1000,
+        cursor: 'pointer',
+        color: '#a3aab5',
+        '&:hover': {
+          color: '#555',
+        },
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        width="50"
+        height="50"
+        fill="currentColor"
+      >
+        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.22 2.2.82a7.68 7.68 0 0 1 2-.27c.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.19 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+      </svg>
+    </Box>
+  )}
+</Box>
     </Box>
   );
 }
